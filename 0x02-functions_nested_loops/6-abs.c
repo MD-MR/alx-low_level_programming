ff90b1b2ced475b8554783aab7daa43fd82f2e09
git <mohammedmudesir44@gmail.com>
2023-07-18 16:15:09 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - give absolute value of a number
+ *
+ * @x: number
+ *
+ * Return: Always 0
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * -1);
+	}
+}
