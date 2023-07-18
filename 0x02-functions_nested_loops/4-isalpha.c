ab2aac88b33a43680bfb2686a591292ecd8cd437
git <mohammedmudesir44@gmail.com>
2023-07-18 15:51:39 -0700

    is it alpha or not

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,7 @@
  */
 int _isalpha(int c)
 {
-	if ((c > 96 && c < 123) && (c > 64 && c < 91))
+	if ((c > 96 && c < 123) || (c > 64 && c < 91))
 	{
 		return (1);
 	}
@@ -17,4 +17,5 @@ int _isalpha(int c)
 	{
 		return (0);
 	}
+	_putchar('\n');
 }
