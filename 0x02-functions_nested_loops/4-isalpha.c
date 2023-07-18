1475c9c9bda8b619a1611261a8ae0438bca44b6a
git <mohammedmudesir44@gmail.com>
2023-07-18 15:45:22 -0700

    is it alpha or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - identify alphaets
+ *
+ * @c: ASCII code
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if ((c > 96 && c < 123) && (c > 64 && c < 91))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
