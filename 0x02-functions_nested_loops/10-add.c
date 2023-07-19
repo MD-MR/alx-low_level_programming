ee9b88c343a4ce53b555f0a74b485dca12889a15
git <mohammedmudesir44@gmail.com>
2023-07-18 18:01:21 -0700

    add 2 nums

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ *
+ *@a: input number
+ *@b: input number
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+
+	return (n);
+}
