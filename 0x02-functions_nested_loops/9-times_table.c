9c43a39e259a8f1f28af88b31a56d2f7c7446d43
git <mohammedmudesir44@gmail.com>
2023-07-18 17:49:10 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - displays times table
+ *
+ * Return: no
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			z = x * y;
+
+			if ((z / 10) == 0)
+			{
+				if (y != 0)
+					_putchar(' ');
+				_putchar(z + 48);
+
+				if (y == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((z / 10) + 48);
+				_putchar((z % 10) + 48);
+				if (y == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
