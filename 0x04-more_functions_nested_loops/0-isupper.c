590ba5891d2e25ee9969b8de1f0a072369a00851
git <mohammedmudesir44@gmail.com>
2023-07-20 12:15:28 -0700

    is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
-* 
+* _isupper - identify uppercase
 *
-* Return: Always 0 (Success)
+* @c: letter input
+*
+* Return: 1 for upper else 0
 */
 int _isupper(int c)
 {
-	if(c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
